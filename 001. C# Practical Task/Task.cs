74242a2c0521dc125845b9550dfe97bdee1b94a1
Khasan Rashidov <khasan_rashidov@mail.ru>
2023-01-07 21:50:25 +0500

    Update Task.cs

--- 001. C# Practical Task/Task.cs
+++ 001. C# Practical Task/Task.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// Practical Task
+// Create a program that takes a sequence of symbols (string) as arguments from the command line 
+// and prints the maximum number of unequal consecutive characters per line to the console.
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace PracticalTask
